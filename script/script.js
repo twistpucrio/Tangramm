86501383cc31751ef919c0552e545ae64f9c76a3
Nandapiquet <nandapiquet@tecgraf.puc-rio.br>
2025-08-25 17:12:07 -0300

    java que as peças se mexem e giram

--- script/script.js
+++ script/script.js
@@ -1,69 +1,76 @@
-$(document).ready(function() {
-        
-        $('.block').draggable({
-            containment:'window',
-            stack: '.block',
-		    snap: true,
-		    snapMode: 'outer',
-		    snapTolerance: 13,
-        });
+$(document).ready(function () {
+    $('.block').draggable({
+        containment: 'window',
+        stack: '.block',
+        snap: true,
+        snapMode: 'outer',
+        snapTolerance: 13,
+    });
 
-    	$('#blockTray').on('mousedown', function () {
-		      $('#instruction').fadeOut('slow');
-	    });
-    
-    // Make blocks rotate 90 deg on each click
-        var angle = 90;    
+    $('#blockTray').on('mousedown', function () {
+        $('#instruction').fadeOut('slow');
+    });
 
-        $('.block').click(function() {
-           
-            $(this).css ({
-                '-webkit-transform': 'rotate(' + angle + 'deg)',
-                   '-moz-transform': 'rotate(' + angle + 'deg)',
-                     '-o-transform': 'rotate(' + angle + 'deg)',
-                    '-ms-transform': 'rotate(' + angle + 'deg)'
-            });
-            angle+=90;
-        });
     let pecaSelecionada = null;
-    $('.block').click(function(e) {
-    // Seleciona a peça clicada
-    pecaSelecionada = $(this);
 
-    // (Opcional) Adiciona uma borda para indicar que está selecionada
-    $('.block').css('box-shadow', 'none');
-    pecaSelecionada.css('box-shadow', '0 0 10px 3px yellow');
-});
-$(document).keydown(function(e) {
-    if (!pecaSelecionada) return; // nenhuma peça selecionada
+    // Rotaciona e aplica flip se for paralelogramo
+    function aplicarTransformacoes(peca, angulo, flip) {
+        const id = peca.attr('id');
+        let transform = `rotate(${angulo}deg)`;
 
-    // Tecla "seta para a direita"
-    if (e.key === "ArrowRight") {
-        let currentAngle = pecaSelecionada.data('angle') || 0;
-        currentAngle = (currentAngle + 45) % 360;
-        pecaSelecionada.data('angle', currentAngle);
+        if (id === 'parallelogram') {
+            if (flip) {
+                transform = `scaleY(-1) rotate(${angulo}deg)`;
+            }
+        }
 
-        pecaSelecionada.css({
-            'transform': 'rotate(' + currentAngle + 'deg)',
-            '-webkit-transform': 'rotate(' + currentAngle + 'deg)',
-            '-moz-transform': 'rotate(' + currentAngle + 'deg)',
-            '-ms-transform': 'rotate(' + currentAngle + 'deg)',
-            '-o-transform': 'rotate(' + currentAngle + 'deg)'
+        peca.css({
+            'transform': transform,
+            '-webkit-transform': transform,
+            '-moz-transform': transform,
+            '-ms-transform': transform,
+            '-o-transform': transform
         });
     }
-});
-if (e.key === "ArrowLeft") {
-    let currentAngle = pecaSelecionada.data('angle') || 0;
-    currentAngle = (currentAngle - 45 + 360) % 360; // evita negativo
-    pecaSelecionada.data('angle', currentAngle);
 
-    pecaSelecionada.css({
-        'transform': 'rotate(' + currentAngle + 'deg)',
-        '-webkit-transform': 'rotate(' + currentAngle + 'deg)',
-        '-moz-transform': 'rotate(' + currentAngle + 'deg)',
-        '-ms-transform': 'rotate(' + currentAngle + 'deg)',
-        '-o-transform': 'rotate(' + currentAngle + 'deg)'
+    // Clique seleciona e gira 45°
+    $('.block').click(function () {
+        pecaSelecionada = $(this);
+        $('.block').css('box-shadow', 'none');
+        pecaSelecionada.css('box-shadow', '0 0 10px 3px yellow');
+
+        let angulo = (pecaSelecionada.data('angle') || 0) + 45;
+        angulo %= 360;
+        pecaSelecionada.data('angle', angulo);
+
+        let flip = pecaSelecionada.data('flip') || false;
+
+        aplicarTransformacoes(pecaSelecionada, angulo, flip);
     });
-}
 
+    // Teclado
+    $(document).keydown(function (e) {
+        if (!pecaSelecionada) return;
+
+        let angulo = pecaSelecionada.data('angle') || 0;
+        let flip = pecaSelecionada.data('flip') || false;
+
+        if (e.key === 'ArrowRight') {
+            angulo = (angulo + 45) % 360;
+        } else if (e.key === 'ArrowLeft') {
+            angulo = (angulo - 45 + 360) % 360;
+        } else if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
+            // Inverte o flip apenas se for o paralelogramo
+            if (pecaSelecionada.attr('id') === 'parallelogram') {
+                flip = !flip;
+            }
+        } else {
+            return;
+        }
+
+        pecaSelecionada.data('angle', angulo);
+        pecaSelecionada.data('flip', flip);
+
+        aplicarTransformacoes(pecaSelecionada, angulo, flip);
+    });
 });
